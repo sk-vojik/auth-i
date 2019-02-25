f0ecfac24f679946f7845a471f05be404a36a931
Scott Vojik <scott.vojik@gmail.com>
2019-02-25 14:46:10 -0800

    restricted login working

--- index.js
+++ index.js
@@ -45,14 +45,51 @@ server.post('/api/login', (req, res) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid credentials' });
+        res.status(401).json({ message: 'You shall not pass' });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     })
-
 });
+
+
+//restriction middleware
+
+function restricted (req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        // check that passwords match
+        if (user && bcrypt.compareSync(password, user.password)) {
+            next()
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'No creds provided' });
+      });
+  } else {
+    res.status(400).json({ message: 'No creds provided' });
+  }
+
+}
+
+//get users
+
+server.get('/api/users', restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+
 
 
 const port = process.env.PORT || 5001;
