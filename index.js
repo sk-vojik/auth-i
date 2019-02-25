3e56637ba26eec9310024e21c527858cd9382a80
Scott Vojik <scott.vojik@gmail.com>
2019-02-25 14:27:52 -0800

    login working

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ server.get('/', (req, res) => {
 });
 
 
-//post
+//post resgister
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
@@ -35,7 +35,24 @@ server.post('/api/register', (req, res) => {
     });
 });
 
+//post login
+server.post('/api/login', (req, res) => {
+  let { username, password } = req.body;
 
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+
+});
 
 
 const port = process.env.PORT || 5001;
