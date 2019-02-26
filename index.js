95618429a494e5d76da1ecff851bceae5913aea7
Scott Vojik <scott.vojik@gmail.com>
2019-02-26 13:02:00 -0800

    cookies implemented, MVP complete

--- index.js
+++ index.js
@@ -2,15 +2,38 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const db = require('./database/dbConfig');
 const Users = require('./users/users-module.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'cookie',    //cookie name. change it from standard sid(session id)
+  secret: 'keep it secret, keep it safe',      //usually would keep this in .env file
+  cookie: {
+    maxAge: 1000 * 60 * 15,      //1 second times 60 - 1 minute * 15 = 15 minute session length
+    secure: false,      //used only for https or not. would be true after development
+  },
+  httpOnly: true,      //cannot access the cookie from js.
+  resave: false,
+  saveUninitialized: false,    // laws against setting cookies automatically
+
+  store: new KnexSessionStore({ 
+    knex: db,
+    tablename: 'sessions',      //all lower case
+    sidfieldname: 'sid',       //all lower case
+    createtable: true,        //all lower case
+    clearInterval: 1000 * 60 * 60       //clean it every hour
+  })
+}
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 //test get
 server.get('/', (req, res) => {
@@ -22,12 +45,13 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
-  const hash = bcrypt.hashSync(user.password, 14);
+  const hash = bcrypt.hashSync(user.password, 14);   //generate hash from user's password
 
-  user.password = hash;
+  user.password = hash;    //override user.password with hash
 
   Users.add(user)
     .then(saved => {
+      req.session.user = user; //registered, so stays logged in
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -56,28 +80,37 @@ server.post('/api/login', (req, res) => {
 
 //restriction middleware
 
-function restricted (req, res, next) {
-  const { username, password } = req.headers;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        // check that passwords match
-        if (user && bcrypt.compareSync(password, user.password)) {
-            next()
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'No creds provided' });
-      });
+function restricted(req, res, next) {
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(400).json({ message: 'No creds provided' });
+    res.status(401).json({ message: 'you shall not pass!' })
   }
-
 }
+
+// function restricted (req, res, next) {
+//   const { username, password } = req.headers;
+
+//   if (username && password) {
+//     Users.findBy({ username })
+//       .first()
+//       .then(user => {
+//         // check that passwords match
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//             next()
+//         } else {
+//           res.status(401).json({ message: 'Invalid Credentials' });
+//         }
+//       })
+//       .catch(error => {
+//         res.status(500).json({ message: 'No creds provided' });
+//       });
+//   } else {
+//     res.status(400).json({ message: 'No creds provided' });
+//   }
+
+// }
 
 //get users
 
@@ -89,6 +122,22 @@ server.get('/api/users', restricted, (req, res) => {
     .catch(err => res.send(err));
 });
 
+
+//LOGOUT
+
+server.get('/api/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send('error')
+      } else {
+        res.send('bye, thanks for coming')
+      }
+    });
+  } else {
+    res.end();
+  }
+});
 
 
 
